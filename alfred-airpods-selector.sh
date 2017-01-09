3a47e1513de343b5177381f5a78658289ab473db
Paul Sri <paulsri@gmail.com>
2017-01-09 13:46:42 -0800

    support multiple airpods

--- alfred-airpods-selector.sh
+++ alfred-airpods-selector.sh
@@ -1,7 +1,8 @@
 toggle=$1
 
 switch="/usr/local/bin/SwitchAudioSource"
-airpods="Paul’s AirPods"
+airpods1="Bedroom AirPods"
+airpods2="Travel AirPods"
 mic="Built-in Microphone"
 output="Built-in Output"
 
@@ -9,6 +10,7 @@ if [ $toggle = "off" ]; then
   $switch -t input -s "$mic" > /dev/null 2>&1 && $switch -t output -s "$output" > /dev/null 2>&1
   echo "Disabled"
 else
-  $switch -t input -s "$airpods" > /dev/null 2>&1 && $switch -t output -s "$airpods" > /dev/null 2>&1
+  $switch -t input -s "$airpods1" > /dev/null 2>&1 && $switch -t output -s "$airpods1" > /dev/null 2>&1
+  $switch -t input -s "$airpods2" > /dev/null 2>&1 && $switch -t output -s "$airpods2" > /dev/null 2>&1
   echo "Enabled"
 fi
