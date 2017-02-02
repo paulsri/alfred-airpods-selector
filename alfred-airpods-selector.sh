b87a3ac246ea0b5cf385b07f04568b102d4eb393
Paul Sri <paulsri@gmail.com>
2017-02-01 19:15:08 -0800

    added 3rd pair

--- alfred-airpods-selector.sh
+++ alfred-airpods-selector.sh
@@ -1,8 +1,9 @@
 toggle=$1
 
 switch="/usr/local/bin/SwitchAudioSource"
-airpods1="Bedroom AirPods"
-airpods2="Travel AirPods"
+airpods1="AirPods Bedroom"
+airpods3="AirPods Office"
+airpods2="AirPods Travel"
 mic="Built-in Microphone"
 output="Built-in Output"
 
@@ -12,5 +13,6 @@ if [ $toggle = "off" ]; then
 else
   $switch -t input -s "$airpods1" > /dev/null 2>&1 && $switch -t output -s "$airpods1" > /dev/null 2>&1
   $switch -t input -s "$airpods2" > /dev/null 2>&1 && $switch -t output -s "$airpods2" > /dev/null 2>&1
+  $switch -t input -s "$airpods3" > /dev/null 2>&1 && $switch -t output -s "$airpods3" > /dev/null 2>&1
   echo "Enabled"
 fi
